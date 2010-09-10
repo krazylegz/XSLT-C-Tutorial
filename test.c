16a7da941bdecf5644093b4b990afa061d2520de
Vikram Dighe <vikram@swiftsignal.com>
2010-09-09 23:05:19 -0400

    Removed unnecessary includes.

--- test.c
+++ test.c
@@ -1,13 +1,4 @@
 #include <string.h>
-#include <libxml/xmlmemory.h>
-#include <libxml/debugXML.h>
-#include <libxml/HTMLtree.h>
-#include <libxml/xmlIO.h>
-#include <libxml/SAX.h>
-#include <libxml/xinclude.h>
-#include <libxml/catalog.h>
-#include <libxslt/xslt.h>
-#include <libxslt/xsltInternals.h>
 #include <libxslt/transform.h>
 #include <libxslt/xsltutils.h>
 
